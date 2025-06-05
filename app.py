862167558d56902e98cd9c7725ddc4514f2ab62c
KEZEIWEI <c111181123@nkust.edu.tw>
2025-06-05 19:46:32 +0800

    fix: render port binding

--- app.py
+++ app.py
@@ -53,4 +53,7 @@ def display_image(filename):
     return f'<img src="/static/uploads/{filename}" width="300">'
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
